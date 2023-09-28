cc416217c6bff8c55378727bd333293b23ca805b
moishy friedman <moshehlf@gmail.com>
2023-09-28 14:24:48 +0300

    components with react-hook-from

--- src/components/RegularForm.tsx
+++ src/components/RegularForm.tsx
@@ -1,67 +1,122 @@
-import  { useState, ChangeEvent, FormEvent } from 'react';
+import { useForm, type FieldValues } from "react-hook-form";
+import { ErrorMessage } from "@hookform/error-message";
 
-interface FormData {
-  username: string;
-  email: string;
-  password:string;
-}
+const onSubmit = (data: FieldValues) => {
+  alert(JSON.stringify(data));
+};
 
-function RegularForm() {
-  const [formData, setFormData] = useState<FormData>({
-    username: '',
-    email: '',
-    password: '',
-  });
-
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
-  };
-
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    alert(JSON.stringify(formData));
-  };
+export default function RegularForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
 
   return (
-    <form onSubmit={handleSubmit}>
-        <h1>Change Me To React Hook Form</h1>
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <h1>Change Me To React Hook Form</h1>
       <div>
         <input
+          {...register("userName", {
+            required: "user name is required",
+            minLength: {
+              value: 2,
+              message: "password most be minimum 2 characters",
+            },
+          })}
           type="text"
-          id="username"
-          name="username"
-          placeholder='Enter UserName'
-          value={formData.username}
-          onChange={handleChange}
+          placeholder="Enter UserName"
         />
+        {errors.userName && (
+          <ErrorMessage
+            errors={errors}
+            name="userName"
+            render={({ message }) => <p>{message}</p>}
+          />
+        )}
       </div>
       <div>
         <input
-          type="text"
-          id="email"
-          name="email"
-          placeholder='Enter Email'
-          value={formData.email}
-          onChange={handleChange}
+          {...register("email", {
+            required: "email is required",
+            pattern: {
+              value: /^\S+@\S+$/i,
+              message: "email must be correct",
+            },
+          })}
+          type="email"
+          placeholder="Enter Email"
         />
+        {errors.email && (
+          <ErrorMessage
+            errors={errors}
+            name="email"
+            render={({ message }) => <p>{message}</p>}
+          />
+        )}
       </div>
       <div>
         <input
+          {...register("password", {
+            required: "password is required",
+            pattern: {
+              value: /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&()*-+=]).+$/,
+              message:
+                "Password must contain at least one uppercase letter, one lowercase letter, and one special character (@, $, !, %, *, #, (, ), ^, =, +, &)",
+            },
+            minLength: {
+              value: 8,
+              message: "password most be minimum 8 characters",
+            },
+            maxLength: {
+              value: 20,
+              message: "password most be maximum 20 characters",
+            },
+          })}
           type="text"
-          id="password"
-          name="password"
-          placeholder='Enter Password'
-          value={formData.password}
-          onChange={handleChange}
+          placeholder="Enter Password"
         />
+        {errors.password && (
+          <ErrorMessage
+            errors={errors}
+            name="password"
+            render={({ message }) => <p>{message}</p>}
+          />
+        )}
+      </div>
+      <div>
+        <input
+          type="number"
+          {...register("age", {
+            min: {
+              value: 18,
+              message: "the age must be minimum 18",
+            },
+            max: {
+              value: 99,
+              message: "the age must be minimum 99",
+            },
+          })}
+        />
+        {errors.age && (
+          <ErrorMessage
+            errors={errors}
+            name="age"
+            render={({ message }) => <p>{message}</p>}
+          />
+        )}
+      </div>
+      <div>
+        <label>
+          Male
+          <input type="radio" value="Male" checked {...register("gender")} />
+        </label>
+        <label>
+          Female
+          <input type="radio" value="Female" {...register("gender")} />
+        </label>
       </div>
       <button type="submit">Submit</button>
     </form>
   );
 }
-
-export default RegularForm;
